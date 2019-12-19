452a02b4229285e85dd9bdecbe58163b9e683257
zkj <438101845@qq.com>
2019-12-19 10:06:06 +0800

    [zhoukangjian] {feat}-{StoryNo.1.3.2}: finish the homework1.3.2's questions1's .js

--- course1/week2/js/section2-question1.js
+++ course1/week2/js/section2-question1.js
@@ -2,9 +2,11 @@ window.onload = function() {
   var user = document.getElementsByClassName('user');
   var password = document.getElementsByClassName('password');
   var logInButton = document.getElementsByClassName('log-in');
-  logInButton.onclick = function() {
-    if (user['value'] != '123' || password['value' != '123']) {
+  logInButton[0].onclick = function() {
+    if (user[0]['value'] != '123' || password[0]['value'] != '123') {
       alert('用户名或密码错误');
+      user[0]['value'] = '';
+      password[0]['value'] = '';
     }
   }
 };
