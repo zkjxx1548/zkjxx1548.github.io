39a90722644bffd08e6a6e6d601925880841b876
zkj <438101845@qq.com>
2019-12-20 17:05:25 +0800

    [zhoukangjian] {feat}-{StoryNo.1.3.2}: finish the homework1.3.2's questions2's .js and .css

--- course1/week2/js/section2-question2.js
+++ course1/week2/js/section2-question2.js
@@ -1,12 +1,40 @@
 window.onload = function() {
-  var user = document.getElementsByClassName('user');
-  var password = document.getElementsByClassName('password');
-  var logInButton = document.getElementsByClassName('log-in');
-  logInButton[0].onclick = function() {
-    if (user[0]['value'] != '123' || password[0]['value'] != '123') {
-      alert('用户名或密码错误');
-      user[0]['value'] = '';
-      password[0]['value'] = '';
+  var boxs = document.getElementsByClassName('boxs');
+  var boxNomove = boxs[0];
+  var drag = boxs[1];
+  var areas = document.getElementsByClassName('main');
+  var area = areas[0];
+  
+  drag.onmousedown = function(event) {
+    var event = event || window.event;
+    var diffX = event.clientX - drag.offsetLeft;
+    var diffY = event.clientY - drag.offsetTop;
+    if (typeof drag.setCapture !== 'undefined') {
+      drag.setCapture();
+    }
+    document.onmousemove = function(event) {
+      var event = event || window.event;
+      var moveX = event.clientX - diffX;
+      var moveY = event.clientY - diffY;
+      if (moveX < 0) {
+        moveX = 0;
+      }else if (moveX > area.offsetWidth - drag.offsetWidth) {
+        moveX = area.offsetWidth - drag.offsetWidth;
+      }
+      if (moveY < 0) {
+        moveY = 0;
+      }else if (moveY > area.offsetHeight - drag.offsetHeight) {
+        moveY = area.offsetHeight - drag.offsetHeight;
+      }
+      if (moveX > 640 && moveX < 800 && moveY > 440 && moveY < 600) {
+        boxNomove.style.backgroundColor = 'blue';
+      }
+      drag.style.left = moveX + 'px';
+      drag.style.top = moveY + 'px';
+    }
+    document.onmouseup = function(event) {
+      this.onmousedown = null;
+      this.onmousemove = null;
     }
   }
 };
