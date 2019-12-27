5599d5f97f23139632b16ea3e9befb582f7c42b8
zkj <438101845@qq.com>
2019-12-27 18:58:00 +0800

    [zhoukangjian] {fix}-{StoryNo.1.3.2}: adjust the question2.js

--- course1/week2/section2/js/question2.js
+++ course1/week2/section2/js/question2.js
@@ -1,40 +1,69 @@
 window.onload = function() {
-  var boxs = document.getElementsByClassName('boxs');
-  var boxNomove = boxs[0];
-  var drag = boxs[1];
-  var areas = document.getElementsByClassName('main');
-  var area = areas[0];
-  
-  drag.onmousedown = function(event) {
+  const boxNomove = this.document.getElementById('no-move');
+  const boxMove = this.document.getElementById('move');
+  const area = document.getElementById('main');
+  var drag = 0;
+  var diffX = 0;
+  var diffY = 0;
+  var boxNomoveLeft = parseInt(this.document.defaultView.getComputedStyle(boxNomove, null).left);
+  var boxNomoveTop = parseInt(this.document.defaultView.getComputedStyle(boxNomove, null).top);
+  var boxsWidth = parseInt(this.document.defaultView.getComputedStyle(boxNomove, null).width);
+  var boxsHeight = parseInt(this.document.defaultView.getComputedStyle(boxNomove, null).height);
+
+
+  boxMove.onmousedown = function (event) {
     var event = event || window.event;
-    var diffX = event.clientX - drag.offsetLeft;
-    var diffY = event.clientY - drag.offsetTop;
-    if (typeof drag.setCapture !== 'undefined') {
-      drag.setCapture();
-    }
-    document.onmousemove = function(event) {
+    diffX = event.clientX - boxMove.offsetLeft;
+    diffY = event.clientY - boxMove.offsetTop;
+    drag = 1;
+    if (typeof boxMove.setCapture !== 'undefined')
+      boxMove.setCapture();
+  }
+
+  document.onmousemove = function (event) {
+    if (drag === 1) {
       var event = event || window.event;
       var moveX = event.clientX - diffX;
       var moveY = event.clientY - diffY;
-      if (moveX < 0) {
-        moveX = 0;
-      }else if (moveX > area.offsetWidth - drag.offsetWidth) {
-        moveX = area.offsetWidth - drag.offsetWidth;
-      }
-      if (moveY < 0) {
-        moveY = 0;
-      }else if (moveY > area.offsetHeight - drag.offsetHeight) {
-        moveY = area.offsetHeight - drag.offsetHeight;
-      }
-      if (moveX > 640 && moveX < 800 && moveY > 440 && moveY < 600) {
+      moveX = isMoveBoxCrossBorderX(moveX);
+      moveY = isMoveBoxCrossBorderY(moveY);
+      if (changeNomoveBoxBlue(moveX, moveY)) {
         boxNomove.style.backgroundColor = 'blue';
       }
-      drag.style.left = moveX + 'px';
-      drag.style.top = moveY + 'px';
+      boxMove.style.left = moveX + 'px';
+      boxMove.style.top = moveY + 'px';
     }
-    document.onmouseup = function(event) {
-      this.onmousedown = null;
-      this.onmousemove = null;
+  }
+
+  document.onmouseup = function () {
+    drag = 0;
+    this.onmousedown = null;
+    this.onmousemove = null;
+  }
+
+  function isMoveBoxCrossBorderX(moveX) {
+    if (moveX < 0) {
+      moveX = 0;
+    }else if (moveX > area.offsetWidth - boxMove.offsetWidth) {
+      moveX = area.offsetWidth - boxMove.offsetWidth;
     }
+    return moveX;
+  }
+
+  function isMoveBoxCrossBorderY(moveY) {
+    if (moveY < 0) {
+      moveY = 0;
+    }else if (moveY > area.offsetHeight - boxMove.offsetHeight) {
+      moveY = area.offsetHeight - boxMove.offsetHeight;
+    }
+    return moveY;
+  }
+
+  function changeNomoveBoxBlue(x, y) {
+    if (
+      x > boxNomoveLeft - boxsWidth && x < boxNomoveLeft + boxsWidth &&
+      y > boxNomoveTop - boxsHeight && y < boxNomoveTop + boxsHeight      
+      )
+      return true;
   }
 };
